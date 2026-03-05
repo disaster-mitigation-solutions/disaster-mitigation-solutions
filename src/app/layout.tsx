3eea8a24a91ff032a85f1c29cd5e090a77079e9c
alice <alice@3mor.io>
2026-03-05 04:12:26 +0000

    Remove firebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type {Metadata} from 'next';
 import './globals.css';
-import { FirebaseClientProvider } from '@/firebase';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
@@ -21,10 +20,8 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased selection:bg-accent/30">
-        <FirebaseClientProvider>
           {children}
           <Toaster />
-        </FirebaseClientProvider>
       </body>
     </html>
   );
